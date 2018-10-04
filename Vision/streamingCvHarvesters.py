79e331a1b3059136de135da9cacd8981aefcde5b
jaycosaur <jaycorichter@gmail.com>
2018-10-04 19:52:22 +1000

    new test

--- Vision/streamingCvHarvesters.py
+++ Vision/streamingCvHarvesters.py
@@ -27,6 +27,7 @@ while(i<1000):
     buffer = cam.fetch_buffer()
     image = buffer.payload.components[0].data
     print(image)
+    buffer.queue()
     #cv2.imshow("Livestream", image.copy())	
     #cv2.waitKey(1)
     #print("Count: ", numberCars, " Frame: ", i, " FPS: ", 1.0/(time.time()-lastTime), "RES: ", w," x ", h)
