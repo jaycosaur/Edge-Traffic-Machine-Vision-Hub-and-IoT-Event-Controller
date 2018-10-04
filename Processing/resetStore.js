6a74b4b5ddde3551a12b3531b3aee0dd8ded8ff8
jaycosaur <jaycorichter@gmail.com>
2018-10-04 14:22:44 +1000

    Added Server Store Reset

--- Processing/resetStore.js
+++ Processing/resetStore.js
@@ -4,8 +4,9 @@ const config = require('./../config.json')
 // With async/await:
 async function example (src, dest) {
     try {
-      await fs.remove(config.STORE_PATH.slice(0, -1))
-      await fs.ensureDir(config.STORE_PATH.slice(0, -1))
+      const STORE_PATH = config.STORE_PATH
+      await fs.remove(STORE_PATH.slice(0, -1))
+      await fs.ensureDir(STORE_PATH.slice(0, -1))
       await fs.ensureDir(config.RAW_STORE_PATH.slice(0, -1))
       await fs.ensureDir(config.STAGED_STORE_PATH.slice(0, -1))
       await fs.ensureDir(config.PROCESSED_STORE_PATH.slice(0, -1))
