88358e616756cc0de52c58a2f92a9e2f95ae34eb
jaycosaur <jaycorichter@gmail.com>
2018-10-04 14:37:27 +1000

    Updated

--- Processing/resetStore.js
+++ Processing/resetStore.js
@@ -10,10 +10,10 @@ async function example (src, dest) {
     try {
       const STORE_PATH = config.STORE_PATH
       await fs.remove(STORE_PATH.slice(0, -1))
-      await fs.ensureDir(STORE_PATH.slice(0, -1),options)
-      await fs.ensureDir(config.RAW_STORE_PATH.slice(0, -1),options)
-      await fs.ensureDir(config.STAGED_STORE_PATH.slice(0, -1),options)
-      await fs.ensureDir(config.PROCESSED_STORE_PATH.slice(0, -1),options)
+      await fs.ensureDir(STORE_PATH.slice(0, -1))
+      await fs.ensureDir(config.RAW_STORE_PATH.slice(0, -1))
+      await fs.ensureDir(config.STAGED_STORE_PATH.slice(0, -1))
+      await fs.ensureDir(config.PROCESSED_STORE_PATH.slice(0, -1))
       console.log('Successfully reset store on server')
     } catch (err) {
       console.error(err)
