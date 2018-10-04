bebfd099378bfbd4e2eaa18cfb403073d99e5289
jaycosaur <jaycorichter@gmail.com>
2018-10-04 17:31:07 +1000

    Added Harvesters script

--- Vision/testHarvesters.py
+++ Vision/testHarvesters.py
@@ -6,6 +6,12 @@ h.add_cti_file('/usr/local/lib/baumer/libbgapi2_gige.cti')
 
 h.update_device_info_list()
 
-h.device_info_list
+print(h.device_info_list)
 
 iam = h.create_image_acquisition_manager(serial_number='QG0170070016')
+
+iam.start_image_acquisition()
+
+buffer = iam.fetch_buffer()
+
+print(buffer)
