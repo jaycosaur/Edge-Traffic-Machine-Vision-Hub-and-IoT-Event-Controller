6d4ee768667ee03a746f52805b298523aea2130f
jaycosaur <jaycorichter@gmail.com>
2018-10-04 22:29:27 +1000

    updates

--- Vision/streamingCamerasMainStart.py
+++ Vision/streamingCamerasMainStart.py
@@ -87,9 +87,9 @@ def worker(camId):
             if( what == 'car' ):
                 print(output)
                 numberCars += 1
-                urllib.request.urlopen(TRIGGER_FAR_URL).read()
-                urllib.request.urlopen(TRIGGER_CLOSE_URL).read()
-                urllib.request.urlopen(TRIGGER_TRUCK_URL).read()
+                urllib.request.urlopen(TRIGGER_FAR_FLASH_URL).read()
+                urllib.request.urlopen(TRIGGER_CLOSE_FLASH_URL).read()
+                urllib.request.urlopen(TRIGGER_TRUCK_FLASH_URL).read()
 
         if IS_ROTATE:
             cv2.imshow(CAM_NAME, np.rot90(image.copy()))
