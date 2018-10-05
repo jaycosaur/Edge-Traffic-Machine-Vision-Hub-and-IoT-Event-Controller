160c5ceacf19b553f50a80fb1dd081e9d5d21f04
jaycosaur <jaycorichter@gmail.com>
2018-10-05 13:25:05 +1000

    updates

--- Vision/newStreaming.py
+++ Vision/newStreaming.py
@@ -102,10 +102,10 @@ def worker(camId):
 
 
         cv2.line(rgb, (200,0), (200, w1), (255,255,0), 1)
-        cv2.putText(rgb, 'Up-Road', (200, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,0))
+        cv2.putText(rgb, 'Up-Road', (200, 50), cv2.FONT_HERSHEY_COMPLEX, 0.2, (255,255,0))
 
         cv2.line(rgb, (100,0), (100, w1), (255,255,0), 1)
-        cv2.putText(rgb, 'Close', (100, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,0))
+        cv2.putText(rgb, 'Close', (100, 50), cv2.FONT_HERSHEY_COMPLEX, 0.2, (255,255,0))
 
 
         for cat, score, bounds in results:
