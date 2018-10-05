740a5d9eef0f0f0cebe5db8a58781348f558d1f6
jaycosaur <jaycorichter@gmail.com>
2018-10-05 15:50:01 +1000

    updates

--- Vision/newStreaming.py
+++ Vision/newStreaming.py
@@ -110,18 +110,18 @@ def worker(camId):
             img2 = Image(img)
             results = net.detect(img2)
 
-            #if camId=='CAM_2':
+            if camId=='CAM_2':
 
-                #cv2.line(rgb, (uproadThresh,0), (uproadThresh, w1), (255,255,0), 1)
-                #cv2.putText(rgb, 'Up-Road', (uproadThresh, 50), cv2.FONT_HERSHEY_COMPLEX, 0.2, (255,255,0))
+                cv2.line(rgb, (uproadThresh,0), (uproadThresh, w1), (255,255,0), 1)
+                cv2.putText(rgb, 'Up-Road', (uproadThresh, 50), cv2.FONT_HERSHEY_COMPLEX, 0.2, (255,255,0))
 
-                #cv2.line(rgb, (truckThresh,0), (truckThresh, w1), (255,255,0), 1)
-                #cv2.putText(rgb, 'Truck', (truckThresh, 50), cv2.FONT_HERSHEY_COMPLEX, 0.2, (255,255,0))
+                cv2.line(rgb, (truckThresh,0), (truckThresh, w1), (255,255,0), 1)
+                cv2.putText(rgb, 'Truck', (truckThresh, 50), cv2.FONT_HERSHEY_COMPLEX, 0.2, (255,255,0))
 
-                #cv2.line(rgb, (closeThresh,0), (closeThresh, w1), (255,255,0), 1)
-                #cv2.putText(rgb, 'Close', (closeThresh, 50), cv2.FONT_HERSHEY_COMPLEX, 0.2, (255,255,0))
+                cv2.line(rgb, (closeThresh,0), (closeThresh, w1), (255,255,0), 1)
+                cv2.putText(rgb, 'Close', (closeThresh, 50), cv2.FONT_HERSHEY_COMPLEX, 0.2, (255,255,0))
 
-                #cv2.line(rgb, (0,rightBound), (h1, rightBound), (255,255,255), 1)
+                cv2.line(rgb, (0,rightBound), (h1, rightBound), (255,255,255), 1)
 
             if camId=='CAM_1':
                 cv2.line(rgb, (extraThresh,0), (extraThresh, w1), (255,255,0), 1)
