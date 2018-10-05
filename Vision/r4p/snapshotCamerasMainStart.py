08c9ea8caf55c2d05082b4ec0b75fabb99952a4f
jaycosaur <jaycorichter@gmail.com>
2018-10-05 19:32:51 +1000

    updates

--- Vision/r4p/snapshotCamerasMainStart.py
+++ Vision/r4p/snapshotCamerasMainStart.py
@@ -157,7 +157,10 @@ def worker(camId):
             cv2.imwrite(CACHE_PATH+imageName,im.copy())
             print('Camera ', WINDOW_NAME, ' was triggered at ', time.time())
             lastTime = time.time()
+            
+            stream.push_buffer(buffer)
         cv2.waitKey(1)
+
     cam.stop_acquisition ()
 
 if __name__ == '__main__':
