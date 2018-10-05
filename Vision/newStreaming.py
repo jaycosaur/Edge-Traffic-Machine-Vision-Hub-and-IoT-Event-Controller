135259c91c9d06a181df9f36bbde6874ef2cae25
jaycosaur <jaycorichter@gmail.com>
2018-10-05 12:07:09 +1000

    updates

--- Vision/newStreaming.py
+++ Vision/newStreaming.py
@@ -94,8 +94,8 @@ def worker(camId):
 
         for cat, score, bounds in results:
                 x, y, w, h = bounds
-                cv2.rectangle(img, (int(x-w/2),int(y-h/2)),(int(x+w/2),int(y+h/2)),(255,0,0))
-                cv2.putText(img, str(cat.decode("utf-8")), (int(x), int(y)), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0))
+                cv2.rectangle(rgb, (int(x-w/2),int(y-h/2)),(int(x+w/2),int(y+h/2)),(255,0,0))
+                cv2.putText(rgb, str(cat.decode("utf-8")), (int(x), int(y)), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0))
 
         #predictions = pyyolo.detect(w, h, c, data, thresh, hier_thresh)
 
@@ -137,9 +137,9 @@ def worker(camId):
 
 
         if IS_ROTATE:
-            cv2.imshow(WINDOW_NAME, img)
+            cv2.imshow(WINDOW_NAME, rgb)
         else:
-            cv2.imshow(WINDOW_NAME, img)
+            cv2.imshow(WINDOW_NAME, rgb)
 
         cv2.waitKey(1)
         buffer.queue()
