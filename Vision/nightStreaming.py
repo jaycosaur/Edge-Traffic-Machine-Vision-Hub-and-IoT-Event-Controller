88c64573f1d502748c4f05ef9215f2441b8476c1
jaycosaur <jaycorichter@gmail.com>
2018-10-05 19:52:44 +1000

    updates

--- Vision/nightStreaming.py
+++ Vision/nightStreaming.py
@@ -137,7 +137,7 @@ def worker(camId):
                 if numPixels > 300:
                     mask = cv2.add(mask, labelMask) """
 
-            cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+            cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             cnts = cnts[0] if imutils.is_cv2() else cnts[1]
             cnts = contours.sort_contours(cnts)[0]
             
