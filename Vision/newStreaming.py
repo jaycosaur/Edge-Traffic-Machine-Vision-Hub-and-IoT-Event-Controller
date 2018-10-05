b5790add81cc0397b1e1d3aa6be3dc3a1237ccec
jaycosaur <jaycorichter@gmail.com>
2018-10-05 12:16:36 +1000

    updates

--- Vision/newStreaming.py
+++ Vision/newStreaming.py
@@ -79,7 +79,7 @@ def worker(camId):
         #img = rgb.transpose(2,0,1)
         if IS_ROTATE:
              img = np.rot90(rgb)
-             rgb = np.rot90(rgb)
+             #rgb = np.rot90(rgb)
         else:
             img = rgb
         #print(rgb.shape)
@@ -87,7 +87,7 @@ def worker(camId):
         #c, h, w = img.shape[2], img.shape[1], img.shape[0]
         #data = img.ravel()/255.0
         #data = np.ascontiguousarray(data, dtype=np.float32)
-        img2 = Image(img)
+        img2 = Image(rgb)
         print('here')
         results = net.detect(img2)
         print('here2')   
