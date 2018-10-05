b7dab1682aa5e1ad951ce23ede7129152566ec98
jaycosaur <jaycorichter@gmail.com>
2018-10-05 15:52:22 +1000

    updates

--- Vision/newStreaming.py
+++ Vision/newStreaming.py
@@ -150,7 +150,7 @@ def worker(camId):
                     #x1,y1,x2,y2 = [int(x+w/2),int(y+h/2),int(x-w/2),int(y-h/2)]
                     #cv2.rectangle(rgb, (int(x-w/2),int(y-h/2)),(int(x+w/2),int(y+h/2)),(255,0,0))
                     #cv2.line(rgb, (x1,y1), (x1, y2), color, 2)
-                    #cv2.rectangle(rgb, (x1,y1),(x2,y2),color)
+                    cv2.rectangle(rgb, (x1,y1),(x2,y2),color)
                     #cv2.putText(rgb, str(cat.decode("utf-8")), (int(x), int(y)), cv2.FONT_HERSHEY_COMPLEX, 1, color)
 
                     #simple trigger
