81623184e631ff79ed6cbb91cfb3241ad6858dd8
jaycosaur <jaycorichter@gmail.com>
2018-10-09 20:44:46 +1100

    New Settings

--- Vision/newStreaming.py
+++ Vision/newStreaming.py
@@ -292,7 +292,7 @@ def openCvWorker(camId):
 
     uproadThresh = 220 
     truckThresh = 160
-    closeThresh = 105
+    closeThresh = 100
     extraThresh = 50
     leftBound = 50
     leftBound2 = 60
