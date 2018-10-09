9e22c5cf7252a399c97573ffe78e3cc9bc1f5446
jaycosaur <jaycorichter@gmail.com>
2018-10-09 11:56:13 +1100

    Updates

--- Upload/index.js
+++ Upload/index.js
@@ -122,8 +122,7 @@ const main = async () => {
         let ref = firestore.doc(`records/${record.ID}`)
         const res = await ref.set(record)
         if(res){
-            errorqueue.push(record)
-            console.log(res)
+            //errorqueue.push(record)
         }
         const timeRemaining = calculateTimeRemainingInMS(moment(),uploadStartTime,recordIndex/numberOfFilesInMetaData)
         console.log(`${recordIndex} out of ${numberOfFilesInMetaData} ( ${Math.round(100*(recordIndex/numberOfFilesInMetaData))}%) | ${errorqueue.length} Error Records | Started: ${uploadStartTime.format('LLLL')} | Est. Time Remaining: ${Math.round(timeRemaining/(1000*60))} minutes| Est. Completed Time: ${uploadStartTime.add(timeRemaining, 'ms').format('LLLL')}`)
