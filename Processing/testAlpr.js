23811b4ec776e962622bbd49ac9add96886e4275
jaycosaur <jaycorichter@gmail.com>
2018-10-10 14:39:07 +1100

    Added Open ALPR

--- Processing/testAlpr.js
+++ Processing/testAlpr.js
@@ -1,8 +1,8 @@
-import extractPlateFromImage from './utils/extractPlateFromImage'
+const extractPlateFromImage = require('./utils/extractPlateFromImage')
 
 path = '~/Desktop/ID=e40bcb0b-9fc6-4e58-810c-b98d949d6447_CAM=CAM9_PLATE=ERROR_UNIX=1539120126081.png'
 
-extractPlateFromImage(imageInt, path, ((plate, time, id)=>{
+extractPlateFromImage(1, path, ((plate, time, id)=>{
     // move to new path with plate appended to name
     if(plate){
         console.log(plate)
