96bf2c62db852e4b159d10b5db7c2d153ef95d32
jaycosaur <jaycorichter@gmail.com>
2018-10-12 13:08:56 +1100

    Updates

--- Upload/index.js
+++ Upload/index.js
@@ -512,7 +512,7 @@ const main = async () => {
     await firestore.doc(`batches/${batchUuid}`).set({
         batchId: batchUuid,
         numberOfImages: numberOfImages,
-        numberOfVehicles: storeWithImages.filter(i=>i.CAM==CAM9).length, // FIX LATER ALLIGATOR
+        numberOfVehicles: storeWithImages.filter(i=>i['CAM']==CAM9).length, // FIX LATER ALLIGATOR
         numberOfRecords: storeWithImages.length,
         farProcessingGroupCount: farProcessingGroup,
         closeProcessingGroupCount: closeProcessingGroup,
