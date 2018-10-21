ec7eb8782b31cf95f41fc05fa9da1dae45bbe106
jaycosaur <jaycorichter@gmail.com>
2018-10-21 18:48:36 +1100

    Updates

--- Processing/actions/index.js
+++ Processing/actions/index.js
@@ -10,6 +10,19 @@ const axios = require('axios')
 const encode = require('./../utils/addChunkToPng')
 const processedRecordLog = new logWriter({path: config.PROCESSED_LOGS_PATH})
 
+const workerpool = require('workerpool')
+const pool = workerpool.pool()
+
+const hashingWorker = (data, pathin, pathout) => {
+    encode(data, pathin, pathout,
+        () => {
+            fs.unlink(pathin,(err)=>{
+                if (err) console.log(err);
+            })
+        }
+    )
+}
+
 const convertNameToObj = (meta) => {
     const fileType = `.${meta.split('.')[1]}`
     const parts = meta.split('.')[0].split("_")
@@ -24,7 +37,7 @@ const convertNameToObj = (meta) => {
 
 module.exports = actionHandler = (action) => {
     //console.log(chalk.black.bgYellow('Action Received: ', action.type))
-    if(action.type === actionTypes.rawStoreFileUpdated){
+    if(false && action.type === actionTypes.rawStoreFileUpdated){
         const pathComps = action.payload.path.split("/")
         const { CAM, UNIX, fileType, ID, PLATE, fileName } = convertNameToObj(pathComps[pathComps.length-1])
         axios.get('http://192.168.1.100:8000/gps-coords')
@@ -56,6 +69,37 @@ module.exports = actionHandler = (action) => {
                         })
                     }
                 )
+            }).catch(err=>console.log(err))     
+    }
+    if(action.type === actionTypes.rawStoreFileUpdated){
+        const pathComps = action.payload.path.split("/")
+        const { CAM, UNIX, fileType, ID, PLATE, fileName } = convertNameToObj(pathComps[pathComps.length-1])
+        axios.get('http://192.168.1.100:8000/gps-coords')
+            .then(function (response) {
+                const { lat, lon, time } = response.data
+                const objToWrite = {
+                    ID: ID,
+                    timeUNIX: UNIX,
+                    timeISO: moment.unix(Math.round(UNIX/1000)).toISOString(),
+                    timeGPS: time,
+                    GPS_COORDS: `${lat}, ${lon}`,
+                    CAM,
+                    PLATE,
+                    PATH: fileName
+                }
+                processedRecordLog.write(objToWrite)
+                return response.data
+            }).then(data => {
+                const dataToWrite = {
+                    direction_of_travel: "west",
+                    gps_latitude: data.lat,
+                    gps_longitude: data.lon,
+                    gps_time_iso: data.time,
+                    capture_time_unixms: UNIX,
+                }
+                const pathin = action.payload.path
+                const pathout = `${config.PROCESSED_STORE_PATH}ID=${ID}_CAM=${CAM}_PLATE=${'ERROR'}_UNIX=${UNIX}${fileType}`
+                pool.exec(hashingWorker, [dataToWrite, pathin, pathout])
             }).catch(err=>console.log(err))     
     }
     if(action.type === actionTypes.processedStoreFileUpdated){
