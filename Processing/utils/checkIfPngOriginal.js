e5254a93aa439f958199b845364b6b5a1f59d424
jaycosaur <jaycorichter@gmail.com>
2018-10-21 13:15:05 +1100

    Added script to check security tags and ensure is still original image

--- Processing/utils/checkIfPngOriginal.js
+++ Processing/utils/checkIfPngOriginal.js
@@ -8,6 +8,8 @@ const sha256 = require('sha256')
  
 const buffer = fs.readFileSync(path.join(__dirname, process.argv[2]))
 const chunks = extract(buffer)
+
+let newSecInd = undefined
  
 const textChunks = chunks.filter(function (chunk) {
   return chunk.name === 'tEXt'
@@ -15,9 +17,18 @@ const textChunks = chunks.filter(function (chunk) {
   return text.decode(chunk.data)
 })
 
-console.log(textChunks)
+console.log('META DATA STORED ON IMAGE:')
+
+Object.keys(textChunks).map(key=>console.log(`${key}: ${textChunks[key]}`))
+
+console.log(`SECURITY INDICATOR IN META DATA: ${textChunks["security_indicator"]}`)
 
 new PNG(buffer).decode((pixels)=>{
-    securityIndicator = sha256(pixels.toString('utf8'))
-    console.log(securityIndicator)
+    newSecInd = sha256(pixels.toString('utf8'))
 })
+
+console.log(`SECURITY INDICATOR ON IMAGE: ${newSecInd}`)
+
+console.log(newSecInd===textChunks["security_indicator"]?`SECURITY INDICATORS MATCH, PASS`:`SECURITY INDICATORS DO NOT MATCH, FAIL`)
+
+
