bf6fcf6626a65673c23ba8c1e94159cccea9ef76
jaycosaur <jaycorichter@gmail.com>
2018-10-23 19:26:40 +1100

    updates

--- Vision/streamingCamerasNonMulti.py
+++ Vision/streamingCamerasNonMulti.py
@@ -249,10 +249,10 @@ def mainWorker(camId):
         #print(dir(cam.device.node_map))
         #cam.device.node_map.ExposureTime.value = exp
 
-        print("GAINAUTO ", dir(cam.device.node_map.GainAuto))
-        print("EXPAUTO ", dir(cam.device.node_map.ExposureAuto))
-        print("ExposureTime ", dir(cam.device.node_map.ExposureTime))
-        print("GAIN ", dir(cam.device.node_map.Gain))
+        print("GAINAUTO ", dir(cam.device.node_map.GainAuto.get_int_value))
+        print("EXPAUTO ", dir(cam.device.node_map.ExposureAuto.get_int_value))
+        print("ExposureTime ", dir(cam.device.node_map.ExposureTime.list_of_valid_values))
+        print("GAIN ", dir(cam.device.node_map.Gain.list_of_valid_values))
 
         #cam.device.node_map.GainAuto
         #cam.device.node_map.ExposureAuto
