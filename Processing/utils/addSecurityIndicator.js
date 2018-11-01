71bb5bacab1aceaa701e55bedc724412bdd42c23
jaycosaur <jaycorichter@gmail.com>
2018-11-01 10:50:32 +0100

    Added Security Indicator Write Script

--- Processing/utils/addSecurityIndicator.js
+++ Processing/utils/addSecurityIndicator.js
@@ -7,7 +7,7 @@ const PNG = require('png-js');
 const sha256 = require('sha256')
  
 const fileToSecure = path.join(__dirname, process.argv[2])
-const ouputPath = path.join(__dirname, process.argv[2], ".secure")
+const ouputPath = path.join(__dirname, process.argv[2]+".secure")
 
 addChunkToPng({}, fileToSecure, ouputPath, true, ()=>{
     console.log(chalk.cyan(`SECURITY INDICATOR WRITTEN TO FILE`))
